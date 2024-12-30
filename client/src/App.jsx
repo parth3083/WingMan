e1a7c7d4db15b877488e8696c92ee40bd35cbc45
DOMinator.jsx <parthbrajput30@gmail.com>
2024-12-30 16:09:18 +0530

    React app created and tailwind css added

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="w-full h-screen flex items-center  justify-center text-2xl">
+      Hey my name is Parth and I am full stack web developer and I have Package
+      of 1cr.
+    </div>
+  );
+}
+
+export default App;
